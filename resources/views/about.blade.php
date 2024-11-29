de8c1a2338576b7967043c8763e7c7eacc3f9a12
Hubert Nelson <hubert.checca@tecsup.edu.pe>
2024-11-28 21:08:02 -0500

    Primer commit - inicialización del proyecto Laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'About')
+
+@section('content')
+<div class="py-5 text-center">
+    <h2>Acerca de Nosotros</h2>
+    <p class="lead">Estudio en Tecsup y estoy en la carrera de Diseño y Desarrollo de Software</p>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+// Ruta para la página principal
+Route::get('/', function () {
+    return view('home');
+});
