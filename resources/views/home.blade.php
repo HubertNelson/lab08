596fe4272742a287ca41b71154ac3e1c36c10907
Hubert Nelson <hubert.checca@tecsup.edu.pe>
2024-11-28 22:44:10 -0500

    Upgrading: mejorar estilo del componente en home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Home')
 
 @section('content')
-<div class="py-5 text-center">
-    <h2>Mi página principal</h2>
-    <p class="lead">Este parrafo est parte de el commit 1.</p>
+<div class="container py-5 text-center">
+    <h2 class="text-primary">Mi página principal</h2> <!-- Cambio de color con Bootstrap -->
+    <p class="lead text-muted">Este parrafo est parte de el commit 1.</p> <!-- Cambio de color con Bootstrap -->
 </div>
 @endsection
