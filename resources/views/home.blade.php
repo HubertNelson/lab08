74d8ede5ebd3aded95a0cb2e9e1fc98c5360889c
Hubert Nelson <hubert.checca@tecsup.edu.pe>
2024-11-28 21:58:03 -0500

    modificar texto en la vista home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,6 @@
 @section('content')
 <div class="py-5 text-center">
     <h2>Mi página principal</h2>
-    <p class="lead">Esto es un párrafo dando a conocer los aspectos generales de mi página.</p>
+    <p class="lead">Este parrafo est parte de el commit 1.</p>
 </div>
 @endsection
