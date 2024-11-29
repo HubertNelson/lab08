c7b7b0706e5a86a3aff6df290414aab0eb74eae6
Hubert Nelson <hubert.checca@tecsup.edu.pe>
2024-11-28 22:12:57 -0500

    modificar texto en la vista contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,6 +5,6 @@
 @section('content')
 <div class="py-5 text-center">
     <h2>Contacto</h2>
-    <p class="lead">Mi contacto por correo es: hubert.checca@tecsup.edu.pe</p>
+    <p class="lead">Mi contacto por correo es: hubert.checca@tecsup.edu.pe. Mi numero es: 940039835. Esto es parte de mi coomit 3</p>
 </div>
 @endsection
