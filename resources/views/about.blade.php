70306593008cf8e0b3e3b5c0104c564fac096f68
Hubert Nelson <hubert.checca@tecsup.edu.pe>
2024-11-28 22:09:16 -0500

    modificar texto en la vista about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,6 +5,6 @@
 @section('content')
 <div class="py-5 text-center">
     <h2>Acerca de Nosotros</h2>
-    <p class="lead">Estudio en Tecsup y estoy en la carrera de Diseño y Desarrollo de Software</p>
+    <p class="lead">Estudio en Tecsup y estoy en la carrera de Diseño y Desarrollo de Software, y esto es parte del commit 3</p>
 </div>
 @endsection
